49051bd114256c09750fce06f9ece69bdadf5367
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-17 21:30:45 +0900

    카테고리 랜더링, 카테고리 하위 상세 슬러그 추가

---
+++ app/blog/[category]/[slug]/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// 마 내가 찐 상세다!
+export default function PostDetail() {
+  return <div>PostDetail</div>;
+}
--- app/blog/[category]/page.tsx
+++ app/blog/[category]/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export default function page() {
-  return <div>상세</div>;
+// 카테고리 랜더링 페이지임용 ㅋ
+export default function CategoryListPage() {
+  return <div>CategoryListPage</div>;
 }

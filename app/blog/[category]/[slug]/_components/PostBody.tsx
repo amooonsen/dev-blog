5ca0923a6ec22af2036afb4258bf84dd29a76184
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-19 23:46:30 +0900

    Callout 추가, Callout 호춣 안되는 문제 해결 중

--- app/blog/[category]/[slug]/_components/PostBody.tsx
+++ app/blog/[category]/[slug]/_components/PostBody.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+
+// components
+import { MdxComponents } from '@/components/markdown';
+
+// types
 import { Post } from '@/types/Post';
+
+// libs
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import remarkBreaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm';
+import remarkA11yEmoji from '@fec/remark-a11y-emoji';
 
 interface PostBodyProps {
   post: Post;
 }
 
 export default function PostBody({ post }: PostBodyProps) {
-  return <MDXRemote source={post.content} />;
+  return (
+    <MDXRemote
+      source={post.content}
+      options={{
+        mdxOptions: {
+          remarkPlugins: [remarkGfm, remarkA11yEmoji, remarkBreaks],
+          rehypePlugins: [
+            // pretty code block
+            [
+              // @ts-ignore
+              rehypePrettyCode,
+              {
+                theme: { dark: 'github-dark-dimmed', light: 'github-light' },
+              },
+            ],
+            // toc id를 추가하고 제목을 연결
+            rehypeSlug,
+          ],
+        },
+      }}
+      // components={MdxComponents}
+    />
+  );
 }
---
+++ components/markdown/callout/index.tsx
@@ -0,0 +1,62 @@
+import { PropsWithChildren } from 'react';
+
+import * as Icon from './Icons';
+import { cn } from '@/lib/utils';
+
+type CalloutType = 'info' | 'warn' | 'danger' | 'normal';
+
+interface CalloutProps extends PropsWithChildren {
+  type?: CalloutType;
+  title?: string;
+}
+
+interface IconMetadata {
+  icon: () => JSX.Element;
+  boxClass: string;
+}
+
+// 개선된 메타데이터 타입 정의
+const getIconMetadata = (type: CalloutType): IconMetadata => {
+  switch (type) {
+    case 'info':
+      return {
+        icon: Icon.Warn,
+        boxClass: 'text-informative-foreground bg-informative',
+      };
+    case 'danger':
+      return {
+        icon: Icon.Danger,
+        boxClass: 'text-destructive-foreground bg-destructive',
+      };
+    case 'warn':
+      return {
+        icon: Icon.Info,
+        boxClass: 'text-warning-foreground bg-warning',
+      };
+    case 'normal':
+    default:
+      return {
+        icon: Icon.Normal,
+        boxClass: 'text-secondary-foreground bg-secondary',
+      };
+  }
+};
+
+export const Callout = ({ type = 'normal', title, children }: CalloutProps) => {
+  const { icon: IconComponent, boxClass } = getIconMetadata(type);
+
+  return (
+    <div className={cn('my-6 flex items-center gap-3 rounded-md px-5 py-4', boxClass)}>
+      {type !== 'normal' && (
+        <div>
+          <IconComponent />
+        </div>
+      )}
+
+      <div className="callout-contents flex-1">
+        {title && <span style={{ fontWeight: 'bold' }}>{children}</span>}
+        {children}
+      </div>
+    </div>
+  );
+};

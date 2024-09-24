e39333cc9a6a02893688c735455de613bae5d5d3
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-24 14:30:11 +0900

    메타 데이터 추가

--- app/template.tsx
+++ app/template.tsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+import Header from '@/components/screen/Header';
+import Footer from '@/components/screen/Footer';
+
 // constnats
 // import { slide, opacity, perspective } from '@/constants/pageTransitionConst';
 
@@ -24,7 +27,9 @@ export default function Template({ children }: { children: React.ReactNode }) {
       exit={{ y: 20, opacity: 0 }}
       transition={{ ease: 'easeInOut', duration: 0.75 }}
     >
+      <Header />
       {children}
+      <Footer />
     </motion.div>
   );
 }

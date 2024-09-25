57a252cb79aba94a59dada019cc0c05c9fc78a79
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-09-25 17:39:08 +0900

    알럿 적용
    단, SORT 조건 처리 필요(현재 둘 다 선택되었을 때 모두 초기화 되는 문제 잇음)

--- app/blog/(main)/_components/SortCategoryContainer.tsx
+++ app/blog/(main)/_components/SortCategoryContainer.tsx
@@ -7,6 +7,9 @@ import { useSearchParams, useRouter } from 'next/navigation';
 import { SortCategoryPopover } from './SortCategoryPopover';
 import { Button } from '@/components/ui/button';
 
+// utils
+import { makeDialog } from '@/lib/utils';
+
 interface SortCategoryContainerProps {
   allTags: string[];
 }
@@ -20,32 +23,63 @@ export default function SortCategoryContainer({ allTags }: SortCategoryContainer
     const hasTags = params.has('tags');
     const hasSort = params.has('sort');
 
-    // 전체 초기화
-    if (hasSort && hasTags && window.confirm('전체를 초기화 하시겠습니까?')) {
-      params.delete('tags');
-      params.delete('sort');
-      router.push(`?${params.toString()}`);
-      return;
-    }
+    if (hasTags || hasSort) {
+      if (hasTags && !hasSort) {
+        makeDialog({
+          type: 'alert',
+          title: '초기화 알림',
+          description: '태그를 초기화 하시겠습니까?',
+          cancelText: '취소',
+          confirmText: '확인',
+          onConfirm() {
+            params.delete('tags');
+            router.push(`?${params.toString()}`);
+          },
+        });
+        return;
+      }
 
-    // 태그 초기화
-    if (hasTags && window.confirm('태그를 초기화 하시겠습니까?')) {
-      params.delete('tags');
-      router.push(`?${params.toString()}`);
-      return;
-    }
+      // 정렬만 있을 때 정렬 초기화
+      if (hasSort && !hasTags) {
+        makeDialog({
+          type: 'alert',
+          title: '초기화 알림',
+          description: '정렬을 초기화 하시겠습니까?',
+          cancelText: '취소',
+          confirmText: '확인',
+          onConfirm() {
+            params.delete('sort');
+            router.push(`?${params.toString()}`);
+          },
+        });
+        return;
+      }
 
-    // 정렬 초기화
-    if (hasSort && window.confirm('정렬을 초기화 하시겠습니까?')) {
-      params.delete('sort');
-      router.push(`?${params.toString()}`);
-      return;
+      // 태그와 정렬 둘 다 있을 때
+      if (hasTags && hasSort) {
+        makeDialog({
+          type: 'alert',
+          title: '초기화 알림',
+          description: '모두 초기화 하시겠습니까?',
+          cancelText: '취소',
+          confirmText: '확인',
+          onConfirm() {
+            params.delete('tags');
+            params.delete('sort');
+            router.push(`?${params.toString()}`);
+          },
+        });
+        return;
+      }
     }
 
     // 아무 것도 선택되지 않았을 때 경고
-    if (!hasTags && !hasSort) {
-      window.alert('초기화할 항목이 없습니다.');
-    }
+    makeDialog({
+      type: 'alert',
+      title: '',
+      description: '초기화 할 항목이 없습니다.',
+      confirmText: '확인',
+    });
   };
 
   return (

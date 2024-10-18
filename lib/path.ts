e75ce744177cbe5e75bd43ecdeebbe5e2b27848a
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-18 16:49:18 +0900

    category, slug 찾는 로직 유틸로 분리

---
+++ lib/path.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+
+// 경로로부터 카테고리와 슬러그를 추출하는 함수
+export function extractCategoryAndSlug(
+  postPath: string,
+  basePath: string
+): { category: string; slug: string } {
+  const relativePath = path.relative(basePath, postPath).replace('.mdx', '');
+  const segments = relativePath.split(path.sep);
+  const category = segments[segments.length - 3];
+  const slug = segments[segments.length - 2];
+  return { category, slug };
+}
+
+// 카테고리 이름을 포맷팅하는 함수
+export function formatCategoryName(dirPath: string): string {
+  return dirPath
+    .split('_')
+    .map((token) => token.charAt(0).toUpperCase() + token.slice(1))
+    .join(' ');
+}

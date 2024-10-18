df7239cf839530c9d99bdee5041d757c7a814fcf
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-18 17:18:39 +0900

    캐시 막는 중

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
     return config;
   },
 };

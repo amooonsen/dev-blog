4bcb9d43ad0123ea80e7750a3c987607db706c35
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-18 18:27:47 +0900

    generateStaticParams 추가 및 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    return config;
+  },
+};
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
+
+// import withBundleAnalyzer from '@next/bundle-analyzer';
+
+// withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default withBundleAnalyzer(nextConfig);

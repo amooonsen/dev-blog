7665f0d6bb0a697de19b9c024af2c80610800f7b
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-18 17:21:56 +0900

    next config 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.cache = false;
-    }
+  webpack(config) {
+    config.externals = config.externals || {};
+    config.externals['.next/cache'] = 'commonjs .next/cache';
     return config;
   },
 };

7972d4e786fc3a833d912f10103cc4e1268978a3
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-19 14:22:37 +0900

    정적 랜더링 테스트 중

--- app/[oneDepth]/(main)/page.tsx
+++ app/[oneDepth]/(main)/page.tsx
@@ -4,8 +4,16 @@ import PostListPage from '../../_components/PostListPage';
 // types
 import { ListPageProps } from '@/types/TypePage';
 
+export const dynamic = 'force-static';
+
+// export async function generateStaticParams() {
+//   const paths = [{ oneDepth: 'tech' }, { oneDepth: 'newsletter' }, { oneDepth: 'life' }];
+//   console.log('Static Paths:', paths);
+//   return paths;
+// }
+
 export async function generateStaticParams() {
-  return [{ oneDepth: 'tech' }, { oneDepth: 'newsletter' }, { oneDepth: 'life' }];
+  return [];
 }
 
 const Blog = async ({ params, searchParams }: ListPageProps) => {

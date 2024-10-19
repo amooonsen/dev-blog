5b47305b4efd86e4f512de879ae601b9de8ddba1
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-19 14:28:32 +0900

    뎁스 구조 변경(docs)

--- app/docs/[oneDepth]/[category]/[slug]/loading.tsx
+++ app/docs/[oneDepth]/[category]/[slug]/loading.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import PostHeadSkeleton from '../../../../components/loading/PostHeadSkeleton';
-import PostBodySkeleton from '../../../../components/loading/PostBodySkeleton';
+import PostHeadSkeleton from '../../../../../components/loading/PostHeadSkeleton';
+import PostBodySkeleton from '../../../../../components/loading/PostBodySkeleton';
 
 export default function loading() {
   return (

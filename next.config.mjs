2f938e193c81ee4004717b6528f5836616483def
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-22 11:54:06 +0900

    웹팩 빌드 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,12 @@
 const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     if (config.cache && !dev) {
-      config.cache = Object.freeze({
-        type: 'memory',
-      });
+      config.cache = {
+        type: 'filesystem', // 파일 시스템 캐시 설정으로 수정
+        maxMemoryGenerations: 0,
+      };
     }
-    // Important: return the modified config
+    // 중요: 수정된 구성을 반환해야 합니다.
     return config;
   },
 };

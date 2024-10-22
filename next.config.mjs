b1e97674a2f2ed18b1e19add3f13842b2c0bdb75
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-22 11:40:56 +0900

    캐싱 관련 next config 옵션 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      });
+      config.cache.maxMemoryGenerations = 0;
+    }
+    // Important: return the modified config
     return config;
   },
 };
 
 export default nextConfig;
-
-// import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// withBundleAnalyzer({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default withBundleAnalyzer(nextConfig);

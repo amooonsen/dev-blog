446b8aadd4d65d5749ee1674b1d4ff8a493a27f4
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-11-28 12:18:12 +0900

    푸터 링크 및 아이콘 교체

--- components/ui/floating-dock.tsx
+++ components/ui/floating-dock.tsx
@@ -63,13 +63,14 @@ const FloatingDockMobile = ({
                 }}
                 transition={{ delay: (items.length - 1 - idx) * 0.05 }}
               >
-                <Link
+                <a
+                  target="_blank"
                   href={item.href}
                   key={item.title}
                   className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
                 >
                   <div className="h-4 w-4">{item.icon}</div>
-                </Link>
+                </a>
               </motion.div>
             ))}
           </motion.div>
